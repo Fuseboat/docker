3a9e2ac1ddb51535ea3ce40433bf1be2950e8b71
Bruno Azoulay <azoulaybruno@gmail.com>
2019-04-06 21:46:26 +0200

    Image Alpine Latest

--- alpine/scripts_base/build.sh
+++ alpine/scripts_base/build.sh
@@ -1,13 +1,15 @@
 #!/bin/ash
 
 # Add script function
-source /root/.script_base/base
+source /root/.script_base/base.sh
 
 # time zone docker
 TIME=${TIMEZONE}
+ALPINE_VERSION_REPOS=v${ALPINE_VERSION_REPOS}
+REPOSITORIE_DIR="/etc/apk/repositories"
 
-echo "http://dl-cdn.alpinelinux.org/alpine/v3.8/community" >> /etc/apk/repositories
-echo "http://dl-cdn.alpinelinux.org/alpine/v3.8/main" >> /etc/apk/repositories
+echo "http://dl-cdn.alpinelinux.org/alpine/$ALPINE_VERSION_REPOS/community" >> REPOSITORIE_DIR
+echo "http://dl-cdn.alpinelinux.org/alpine/$ALPINE_VERSION_REPOS/main" >> REPOSITORIE_DIR
 
 # Add pack
 PACK_DEFAULT=" vim zsh openrc htop curl git zip unzip openssh autoconf \
